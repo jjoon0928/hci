eec2abe218f500492fa05cbca0becac3ea102a85
백준영 <junyeong_baek@baegjun-yeong-ui-MacBookAir.local>
2024-05-22 23:12:46 +0900

    first commit

---
+++ NoGlasses_files/spiel.js
@@ -0,0 +1,31 @@
+function modal(modal) {
+	document.getElementById(modal).style.display = "block";
+}
+
+function hideModal(modal) {
+	if (window.event.target.id == modal) {
+		document.getElementById(modal).style.display = "none";
+	}
+}
+
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+$(function() {
+    $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        }
+    });
+});
